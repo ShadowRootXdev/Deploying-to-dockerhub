600bc5ad6a4ad26f8a9527dfc8162640b940bb68
shadowrootxdev <inezabertrand@gmail.com>
2026-02-13 23:19:54 +0200

    changing mongo url

--- Model/Db.js
+++ Model/Db.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = "mongodb://localhost:27017/task";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect("mongodb://localhost:27017/task");
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
--- Model/Employees.js
+++ Model/Employees.js
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 const EmployeeSchema = new mongoose.Schema({
   fullNames: {
     type: String,
-    required: [true, "Full name is required"],
+    required: [true, "Full name "],
   },
   position: {
     type: String,
-    required: [true, "Position is required"],
+    required: [true, "Position "],
   },
   salary: {
     type: Number,
-    required: [true, "Salary is required"],
+    required: [true, "Salary "],
   },
 });
 
